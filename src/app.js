13a8f2a4667b80ea01d269a517c28b3d9189439e
Julian-Lop <you@example.com>
2022-02-11 18:45:11 -0500

    deploy api rest 9

--- src/app.js
+++ src/app.js
@@ -14,13 +14,13 @@ server.use(express.urlencoded({ extended: true, limit: '50mb' }));
 server.use(express.json({ limit: '50mb' })); // Para recibir bien el body. Sin esto llegaría undefined o un objeto vacío.
 server.use(cookieParser()); // Para tener acceso a las cookies.
 server.use(morgan('dev')); // Middleware que sirve para ir mostrando en consola las peticiones que vamos haciendo al servidor.
-server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://julian-lop.github.io/Client-api-dog/'); // update to match the domain you will make the request from
-  res.header('Access-Control-Allow-Credentials', 'true');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
-  next();
-});
+// server.use((req, res, next) => {
+//   res.header('Access-Control-Allow-Origin', 'https://julian-lop.github.io/Client-api-dog/'); // update to match the domain you will make the request from
+//   res.header('Access-Control-Allow-Credentials', 'true');
+//   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+//   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
+//   next();
+// });
 
 server.use('/', routes);
 
