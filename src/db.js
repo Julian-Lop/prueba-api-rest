4b3448560e31e6c469d7163142d5fb2b6a4fece2
Julian-Lop <you@example.com>
2022-02-11 18:49:12 -0500

    deploy api rest 9

--- src/db.js
+++ src/db.js
@@ -6,7 +6,7 @@ const {
   DATABASE_URL
 } = process.env;
 
-const sequelize = new Sequelize(DATABASE_URL, {
+const sequelize = new Sequelize(`${DATABASE_URL}`, {
   dialectOptions: {
     ssl: {
       require: true,
